50c7dd5c9913e9695bf5e6325ae4296aa85e7ef9
ZtenBlack <vladislav@terrasoft.net>
2025-12-11 12:09:50 +0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,49 +8,49 @@ $(document).ready(function () {
         qtag = sessionStorage.getItem('qtag');
     }
     if (qtag) {
-        const newUrl = `https://10black.net/api/affiliate/?qtag=${qtag}`;
-        //const newUrl = `https://10black.net/api/affiliate/?qtag=${qtag}&signup=false`;
+        //const newUrl = `https://10black.net/api/affiliate/?qtag=${qtag}`;
+        const newUrl = `https://10black.net/api/affiliate/?qtag=${qtag}&signup=false`;
         $('.modal__content-link').attr('href', newUrl);
         console.log(newUrl);
     }
     //language selection code
-    // const accessKey = 'd52fa6bf-a8a9-46c3-b195-2fcda479a705';
-    // if (!sessionStorage.getItem('countryChecked')) {
-    //     $.ajax({
-    //         url: 'https://apiip.net/api/check?&accessKey=' + accessKey,
-    //         success: function (result) {
-    //             console.log(result.countryCode);
-    //             sessionStorage.setItem('countryChecked', 'true');
-    //             if (result.countryCode === 'PL') {
-    //                 window.location.href = '/pl';
-    //             } else if (result.countryCode === 'PT') {
-    //                 window.location.href = '/pt';
-    //             } else if (result.countryCode === 'IT') {
-    //                 window.location.href = '/it';
-    //             } else if (result.countryCode === 'DE') {
-    //                 window.location.href = '/de';
-    //             } else if (result.countryCode === 'GR') {
-    //                 window.location.href = '/el';
-    //             } else if (result.countryCode === 'ES') {
-    //                 window.location.href = '/es';
-    //             } else if (result.countryCode === 'FR') {
-    //                 window.location.href = '/fr';
-    //             } else if (result.countryCode === 'KZ') {
-    //                 window.location.href = '/kk';
-    //             } else if (result.countryCode === 'BR') {
-    //                 window.location.href = '/br';
-    //             } else if (result.countryCode === 'RU') {
-    //                 window.location.href = '/ru';
-    //             } else if (result.countryCode === 'RS') {
-    //                 window.location.href = '/sr';
-    //             } else if (result.countryCode === 'TR') {
-    //                 window.location.href = '/tr';
-    //             } else {
-    //                 window.location.href = '/';
-    //             }
-    //         }
-    //     });
-    // }
+    const accessKey = 'd52fa6bf-a8a9-46c3-b195-2fcda479a705';
+    if (!sessionStorage.getItem('countryChecked')) {
+        $.ajax({
+            url: 'https://apiip.net/api/check?&accessKey=' + accessKey,
+            success: function (result) {
+                console.log(result.countryCode);
+                sessionStorage.setItem('countryChecked', 'true');
+                if (result.countryCode === 'PL') {
+                    window.location.href = '/pl';
+                } else if (result.countryCode === 'PT') {
+                    window.location.href = '/pt';
+                } else if (result.countryCode === 'IT') {
+                    window.location.href = '/it';
+                } else if (result.countryCode === 'DE') {
+                    window.location.href = '/de';
+                } else if (result.countryCode === 'GR') {
+                    window.location.href = '/el';
+                } else if (result.countryCode === 'ES') {
+                    window.location.href = '/es';
+                } else if (result.countryCode === 'FR') {
+                    window.location.href = '/fr';
+                } else if (result.countryCode === 'KZ') {
+                    window.location.href = '/kk';
+                } else if (result.countryCode === 'BR') {
+                    window.location.href = '/br';
+                } else if (result.countryCode === 'RU') {
+                    window.location.href = '/ru';
+                } else if (result.countryCode === 'RS') {
+                    window.location.href = '/sr';
+                } else if (result.countryCode === 'TR') {
+                    window.location.href = '/tr';
+                } else {
+                    //window.location.href = '/';
+                }
+            }
+        });
+    }
     //play button code
     $(window).on('load', function () {
         $("#play-btn").on("click", function () {
